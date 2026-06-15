a024ae125fcdf59301d5da4f9dfb298399f3be75
Salaheddine <khlifi.161790@e.unavarra.es>
2026-06-15 12:29:05 +0200

    [Refactor] - Añadida comprobaciondel signo mediante un if

--- src/Quiniela.php
+++ src/Quiniela.php
@@ -7,6 +7,13 @@ class Quiniela
 
     public function gestionarQuiniela(string $instruccion): string
     {
-        return "Signo no válido";
+        $partesIntruccion = explode(' ', trim($instruccion));
+        $signo = strtolower($partesIntruccion[2]);
+
+        if ($signo != '1' || $signo != 'x' || $signo != '2') {
+            return "Signo no válido";
+        }
+
+        return "";
     }
 }
